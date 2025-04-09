001062d734c3d0c34e8ac8988e471fd9f5619f4e
md gulam noxbondy <gulamnoxbondy@gmail.com>
2025-04-09 12:02:40 +0200

    update

--- src/main/java/repository/AppUserRepository.java
+++ src/main/java/repository/AppUserRepository.java
@@ -4,14 +4,16 @@ import entity.AppUser;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface AppUserRepository extends CrudRepository<AppUser, Integer> {
 
 
-    default AppUser findByUsername(String username) {
-        return null;
-    }
+    AppUser findByUsername(String username);
+    List<AppUser> findByRegistrationDateBetween(LocalDate startDate, LocalDate endDate);
+    List<AppUser> findByDetailsId(Long detailsId);
+    AppUser findByDetailsEmailIgnoreCase(String email);
 }
 
